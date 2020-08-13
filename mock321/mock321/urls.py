cfeaa95732af524d4ae390f8b1b3178773a702e0
K. Bhavya Manvitha <66966989+BhavyaKovuru@users.noreply.github.com>
2020-08-13 14:19:19 +0530

    Add files via upload
    urls and view are modified

--- mock321/mock321/urls.py
+++ mock321/mock321/urls.py
@@ -6,5 +6,10 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
-    path('temp/', views.update, name= "temp")
+    path('temp/', views.update, name= "temp"),
+    path('temp1/', views.update1, name= "temp1"),
+    path('temp2/', views.update2, name= "temp2"),
+    path('temp3/', views.update3, name= "temp3"),
+    path('lowTech/', views.index2, name="index2"),
+    path('highTech/', views.index, name="index")
 ]
--- mock321/mock321/views.py
+++ mock321/mock321/views.py
@@ -3,14 +3,7 @@ from pyrebase import pyrebase
 
 
 config = {
-    'apiKey': "", # add your api key here
-    'authDomain': "",
-    'databaseURL': "",
-    'projectId': "",
-    'storageBucket': "",
-    'messagingSenderId': "",
-    'appId': "",
-    'measurementId': ""
+    #add config details
   }
 
 firebase = pyrebase.initialize_app(config)
@@ -24,7 +17,16 @@ def index(request):
         ret[i]=db["grade1"][i]['highTech']
 
     print(ret)
-    return render(request,'index.html',{'mon_h': ret['monday'], 'tue_h':ret['tuesday'],'thrus_h': ret['thursday']})
+    return render(request,'index.html',{'mon_h': ret['monday'], 'tue_h':ret['tuesday'],'wed_h': ret['wednesday'],'thrus_h': ret['thursday'],'fri_h': ret['friday'], 'sat_h': ret['saturday'], 'sun_h': ret['sunday']})
+
+def index2(request):
+    db = database.child('schools').child('abc').get().val()
+    ret={}
+    for i in db["grade1"].keys():
+        ret[i]=db["grade1"][i]['lowTech']
+
+    print(ret)
+    return render(request,'lowTech.html',{'mon_l': ret['monday'], 'tue_l':ret['tuesday'],'wed_l': ret['wednesday'],'thrus_l': ret['thursday'],'fri_l': ret['friday'], 'sat_l': ret['saturday'], 'sun_l': ret['sunday']})
 
 def update(request):
     from datetime import date
@@ -48,25 +50,101 @@ def update(request):
         ret={}
         for i in db1["grade1"].keys():
             ret[i]=db1["grade1"][i][tech]
-        
+        print(ret)
         return ret
 
-    db = database.child('links').get().val()
-    for i in db.keys():
-        print(i)
-    for j in db[i].keys():
-        print(j)
-        for k in db[i][j].keys():
-            print(db[i][j][k])
-            if(db[i][j][k] == "http://127.0.0.1:8000/temp/"):
-                tech=j
-                ret=update_sub(tech,item)
-                return render(request,'temp2.html',{'mon_h': ret['monday'], 'tue_h':ret['tuesday'],'thrus_h': ret['thursday']})
+    tech='highTech'
+    ret=update_sub(tech,item)
+    return render(request,'temp.html',{'mon_h': ret['monday'], 'tue_h':ret['tuesday'],'wed_h': ret['wednesday'],'thrus_h': ret['thursday'],'fri_h': ret['friday'], 'sat_h': ret['saturday'], 'sun_h': ret['sunday']})
 
-            elif(db[i][j][k] == "http://127.0.0.1:8000/temp1/"):
-                tech=j
-                ret=update_sub(tech,item)
-                return render(request,'temp1.html',{'mon_h': ret['monday'], 'tue_h':ret['tuesday'],'thrus_h': ret['thursday']})
+def update1(request):
+    from datetime import date
+    import calendar
+
+    x=list(map(int,str(date.today()).split('-')))
+    y=calendar.day_name[date(x[0],x[1],x[2]).weekday()].lower()
+    print(y)
+    
+
+    item = y
+    def update_sub(tech,item):
+        db1 = database.child('schools').child('abc').get().val()
+
+        val1 = db1['grade1'][item][tech]
+            
+        val = val1 + 1
+        db1 = database.child('schools').child('abc').child('grade1').child(item).child(tech).set(val)
+
+        db1 = database.child('schools').child('abc').get().val()
+        ret={}
+        for i in db1["grade1"].keys():
+            ret[i]=db1["grade1"][i][tech]
+        print(ret)
+        return ret
+
+    tech='highTech'
+    ret=update_sub(tech,item)
+    return render(request,'temp1.html',{'mon_h': ret['monday'], 'tue_h':ret['tuesday'],'wed_h': ret['wednesday'],'thrus_h': ret['thursday'],'fri_h': ret['friday'], 'sat_h': ret['saturday'], 'sun_h': ret['sunday']})
+
+
+def update2(request):
+    from datetime import date
+    import calendar
+
+    x=list(map(int,str(date.today()).split('-')))
+    y=calendar.day_name[date(x[0],x[1],x[2]).weekday()].lower()
+    print(y)
+    
+
+    item = y
+    def update_sub(tech,item):
+        db1 = database.child('schools').child('abc').get().val()
+
+        val1 = db1['grade1'][item][tech]
+            
+        val = val1 + 1
+        db1 = database.child('schools').child('abc').child('grade1').child(item).child(tech).set(val)
+
+        db1 = database.child('schools').child('abc').get().val()
+        ret={}
+        for i in db1["grade1"].keys():
+            ret[i]=db1["grade1"][i][tech]
+        print(ret)
+        return ret
+
+    tech='lowTech'
+    ret=update_sub(tech,item)
+    return render(request,'temp2.html',{'mon_l': ret['monday'], 'tue_l':ret['tuesday'],'wed_l': ret['wednesday'],'thrus_l': ret['thursday'],'fri_l': ret['friday'], 'sat_l': ret['saturday'], 'sun_l': ret['sunday']})
+
+def update3(request):
+    from datetime import date
+    import calendar
+
+    x=list(map(int,str(date.today()).split('-')))
+    y=calendar.day_name[date(x[0],x[1],x[2]).weekday()].lower()
+    print(y)
+    
+
+    item = y
+    def update_sub(tech,item):
+        db1 = database.child('schools').child('abc').get().val()
+
+        val1 = db1['grade1'][item][tech]
+            
+        val = val1 + 1
+        db1 = database.child('schools').child('abc').child('grade1').child(item).child(tech).set(val)
+
+        db1 = database.child('schools').child('abc').get().val()
+        ret={}
+        for i in db1["grade1"].keys():
+            ret[i]=db1["grade1"][i][tech]
+        print(ret)
+        return ret
+
+    
+    tech='lowTech'
+    ret=update_sub(tech,item)
+    return render(request,'temp3.html',{'mon_l': ret['monday'], 'tue_l':ret['tuesday'],'wed_l': ret['wednesday'],'thrus_l': ret['thursday'],'fri_l': ret['friday'], 'sat_l': ret['saturday'], 'sun_l': ret['sunday']})
 
     # else:
     #     tech=j
